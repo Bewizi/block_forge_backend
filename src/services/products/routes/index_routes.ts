ba7f7c4fa1c3bf4cc3e0eba7db43d015bede5fb2
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-10 19:22:32 +0100

    add create products middleware to routes

--- src/services/products/routes/index_routes.ts
+++ src/services/products/routes/index_routes.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
 import Products_controllers from "../controllers/products_controllers";
-import { getProductById } from "../middleware/product_middleware";
+import {
+  createProducts,
+  getProductById,
+} from "../middleware/product_middleware";
 
 const routes = Router();
 
 routes.get("/allProducts", Products_controllers.allProducts);
 // routes.get("/solidProducts", Products_controllers.solidProducts);
 routes.get("/getProduct/:id", getProductById, Products_controllers.getProduct);
-routes.post("/createProduct", Products_controllers.createProduct);
+routes.post(
+  "/createProduct",
+  createProducts,
+  Products_controllers.createProduct
+);
 routes.get("/category/:category", Products_controllers.getProductsByCategory);
 
 export default routes;
