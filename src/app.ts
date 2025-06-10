202df849bd42d96c08f145212566f2f73f42cdf8
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-10 08:40:21 +0100

    add the main root

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,34 @@
-import express from 'express'
-import appConfig from "./shared/config"
+import express from "express";
+import appConfig from "./shared/config";
 import database from "./shared/config/database_config";
 import cors from "cors";
-import {Request, Response} from "express";
+import rootRoutes from "./rootRoutes";
+import * as path from "node:path";
 
-const app = express()
+const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cors({
-    origin: ["*"], // Allow all origins (for development only)
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/public", express.static(path.join(__dirname,  "public",)));
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"], // Allow all origins (for development only)
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-}))
+    credentials: true,
+  }),
+);
 
 // API ROUTES
-app.get('/api/text', (req: Request, res: Response) => {
-    console.log('>>> GET /api/test endpoint hit');
-    console.log('>>> Query params:', req.query);
-    console.log('>>> Headers:', req.headers);
-    
-    console.log('welcome')
-    res.status(200).json({
-        message: 'Hello Welcome!',
-    })
-})
+app.use("/api", rootRoutes);
 
 app.listen(appConfig.app.port, appConfig.app.host, async () => {
-    // await database.sync({force: true})
-    await database.sync({alter: true})
-    console.log(`app listening on ${appConfig.app.port} on ${appConfig.app.host}`);
-
-})
+  // await database.sync({force: true})
+  await database.sync({ alter:true});
+  console.log(
+    `app listening on ${appConfig.app.port} on ${appConfig.app.host}`,
+  );
+});
