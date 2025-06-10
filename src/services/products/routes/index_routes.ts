45f7fbbb3ce976a51a95e2f24f90bc8dd74084d8
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-10 08:48:40 +0100

    add routes to access in frontend

--- src/services/products/routes/index_routes.ts
+++ src/services/products/routes/index_routes.ts
@@ -1,6 +1,13 @@
 import { Router } from "express";
 import Products_controllers from "../controllers/products_controllers";
+import { getProductById } from "../middleware/product_middleware";
 
 const routes = Router();
 
 routes.get("/allProducts", Products_controllers.allProducts);
+// routes.get("/solidProducts", Products_controllers.solidProducts);
+routes.get("/getProduct/:id", getProductById, Products_controllers.getProduct);
+routes.post("/createProduct", Products_controllers.createProduct);
+routes.get("/category/:category", Products_controllers.getProductsByCategory);
+
+export default routes;
