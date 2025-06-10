08475b7cd2d9223c9e590efe46d5e75916f76f32
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-10 08:49:37 +0100

    add auth controllers and middleware

--- src/services/users/controllers/auth_controller.ts
+++ src/services/users/controllers/auth_controller.ts
@@ -1,91 +1,91 @@
-import { NextFunction, Request, Response } from "express";
+import {NextFunction, Request, Response} from "express";
 import User from "../models/user_models";
 import bcrypt from "bcrypt";
 
 class AuthController {
-  register = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const body = req.body;
+    register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const {firstname, lastname, email, phoneNumber, password} = req.body;
 
-      //FIND USER BY THIS
-      const userExit = await User.findOne({
-        where: {
-          email: body.email,
-        },
-      });
+            //FIND USER BY THIS
+            const userExit = await User.findOne({
+                where: {
+                    email:email.email,
+                },
+            });
 
-      // IF USER ALREADY EXIT
-      if (userExit) {
-        return res.status(400).json({
-          message: "User Input Cannot be empty",
-          errors: {
-            firstname: "Firstname is required",
-            email: "This email is already register",
-            password: "Password cannot be empty",
-          },
-        });
-      }
+            // IF USER ALREADY EXIT
+            if (userExit) {
+                res.status(400).json({
+                    message: "User Input Cannot be empty",
+                    errors: {
+                        firstname: "Firstname is required",
+                        email: "This email is already register",
+                        password: "Password cannot be empty",
+                    },
+                });
+            }
 
-      // HASH PASSWORD
-      const hashPassword = await bcrypt.hash(body.password, 10);
+            // HASH PASSWORD
+            const hashPassword = await bcrypt.hash(password, 10);
 
-      // CREATE THE USER
-      const user = await User.create({
-        firstname: body.firstname,
-        lastname: body.lastname,
-        email: body.email,
-        phoneNumber: body.phoneNumber,
-        password: hashPassword,
-      });
-      return res.status(201).json({ message: "User Created", user });
-      next();
-    } catch (e) {
-      console.log(e);
-      return res
-        .status(500)
-        .json({ message: "Registration Failed", error: (e as Error).message });
-    }
-  };
+            // CREATE THE USER
+            const user = await User.create({
+                firstname: firstname,
+                lastname: lastname,
+                email: email,
+                phoneNumber: phoneNumber,
+                password: hashPassword,
+            });
+            res.status(201).json({message: "User Created", user});
+            next();
+        } catch (e) {
+            console.log(e);
+            res
+                .status(500)
+                .json({message: "Registration Failed", error: (e as Error).message});
+        }
+    };
 
-  login = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { email, password } = req.body;
+    login = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+        try {
+            const {email, password} = req.body;
 
-      //     CHECK IF USER EXIT
-      const existingUser = await User.findOne({ where: { email } });
-      if (!existingUser) {
-        return res.status(400).json({
-          message: "User does not exist",
-          errors: { email: "No User Found with this email" },
-        });
-      }
+            //     CHECK IF USER EXIT
+            const existingUser = await User.findOne({where: {email}});
+            if (!existingUser) {
+                return res.status(400).json({
+                    message: "User does not exist",
+                    errors: {email: "No User Found with this email"},
+                });
+            }
 
-      //     CHECK IF PASSWORD MATCHES
-      const ifPasswordMatch = await bcrypt.compare(
-        password,
-        existingUser.password
-      );
-      if (!ifPasswordMatch) {
-        return res.status(401).json({
-          message: "Password does not match",
-          errors: { email: "Incorrect Password" },
-        });
-      }
+            //     CHECK IF PASSWORD MATCHES
+            const ifPasswordMatch = await bcrypt.compare(
+                password,
+                existingUser.password
+            );
+            if (!ifPasswordMatch) {
+                return res.status(401).json({
+                    message: "Password does not match",
+                    errors: {email: "Incorrect Password"},
+                });
+            }
 
-      if (!email || !password) {
-        return res.status(401).json({ message: "Invalid email and password" });
-      }
-      res.status(200).json({ message: "Login Successful" });
+            if (!email || !password) {
+                return res.status(401).json({message: "Invalid email and password"});
+            }
+            res.status(200).json({message: "Login Successful"});
 
-      next();
-    } catch (e) {
-      console.log(e);
-      return res.status(500).json({
-        message: "Logging in User Failed",
-        error: (e as Error).message,
-      });
-    }
-  };
+            next();
+        } catch (e) {
+            console.log(e);
+            return res.status(500).json({
+                message: "Logging in User Failed",
+                error: (e as Error).message,
+            });
+        }
+    };
 }
 
 export default new AuthController();
--- src/services/users/middleware/ath_middleware.ts
+++ src/services/users/middleware/ath_middleware.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import {NextFunction, Request, Response} from "express";
 
 // Regex patterns
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -6,96 +6,102 @@ const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
 const phoneNumberRegex = /^(?:\+234|0)[789]\d{9}$/;
 
 export const registerValidation = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { firstname, lastname, phoneNumber, email, password } = req.body;
+    req: Request,
+    res: Response,
+    next: NextFunction
+): void => {
+    try {
+        const {firstname, lastname, phoneNumber, email, password} = req.body;
 
-    if (!firstname || !lastname) {
-      return res.status(400).json({
-        message: "Firstname and Lastname is required",
-      });
+        if (!firstname || !lastname) {
+            res.status(400).json({
+                message: "Firstname and Lastname is required",
+            });
+            return
+        }
+
+        if (
+            !phoneNumber ||
+            typeof phoneNumber !== "string" ||
+            !phoneNumberRegex.test(phoneNumber)
+        ) {
+            res.status(400).json({
+                message:
+                    "Nigerian phone number is not valid. Format: +234XXXXXXXXXX or 0XXXXXXXXXX",
+            });
+            return
+        }
+
+        if (!email || typeof email !== "string" || !emailRegex.test(email)) {
+            res.status(400).json({message: "Email Address is not valid"});
+            return
+        }
+
+        if (!password) {
+            res.status(404).json({
+                message: "Password is required",
+                errors: {password: "This password cannot be empty"},
+            });
+            return
+        }
+
+        if (
+            typeof password !== "string" ||
+            !passwordRegex.test(password) ||
+            password.length < 8
+        ) {
+            res.status(400).json({
+                message:
+                    "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number",
+                errors: {
+                    password:
+                        "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number",
+                },
+            });
+            return
+        }
+
+        next();
+    } catch (e) {
+        console.log(e);
+        res
+            .status(422)
+            .json({message: "validation failed", error: (e as Error).message});
+        return
     }
-
-    if (
-      !phoneNumber ||
-      typeof phoneNumber !== "string" ||
-      !phoneNumberRegex.test(phoneNumber)
-    ) {
-      return res.status(400).json({
-        message:
-          "Nigerian phone number is not valid. Format: +234XXXXXXXXXX or 0XXXXXXXXXX",
-      });
-    }
-
-    if (!email || typeof email !== "string" || !emailRegex.test(email)) {
-      return res.status(400).json({ message: "Email Address is not valid" });
-    }
-
-    if (!password) {
-      return res.status(404).json({
-        message: "Password is required",
-        errors: { password: "This password cannot be empty" },
-      });
-    }
-
-    if (
-      typeof password !== "string" ||
-      !passwordRegex.test(password) ||
-      password.length < 8
-    ) {
-      return res.status(400).json({
-        message:
-          "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number",
-        errors: {
-          password:
-            "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number",
-        },
-      });
-    }
-
-    next();
-  } catch (e) {
-    console.log(e);
-    return res
-      .status(422)
-      .json({ message: "validation failed", error: (e as Error).message });
-  }
 };
 
 export const loginValidation = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { email, password } = req.body;
+    req: Request,
+    res: Response,
+    next: NextFunction
+): any => {
+    try {
+        const {email, password} = req.body;
 
-    if (!email || !password) {
-      return res
-        .status(400)
-        .json({ message: "Email and Password are required" });
+        if (!email || !password) {
+            return res
+                .status(400)
+                .json({message: "Email and Password are required"});
+        }
+
+        //     IF the email is not a string and doesn't follow the regex pattern
+        if (typeof email !== "string" || !emailRegex.test(email)) {
+            return res.status(400).json({message: "Email is not valid"});
+        }
+
+        // IF the password is empty or not a string and less than 6 characters
+        if (typeof password !== "string" || password.length < 8) {
+            return res.status(400).json({
+                message: "Password must be at least 8 characters long",
+            });
+        }
+
+        next();
+    } catch (e) {
+        console.log(e);
+        res
+            .status(422)
+            .json({message: "validation failed", error: (e as Error).message});
     }
-
-    //     IF the email is not a string and doesn't follow the regex pattern
-    if (typeof email !== "string" || !emailRegex.test(email)) {
-      return res.status(400).json({ message: "Email is not valid" });
-    }
-
-    // IF the password is empty or not a string and less than 6 characters
-    if (typeof password !== "string" || password.length < 8) {
-      return res.status(400).json({
-        message: "Password must be at least 8 characters long",
-      });
-    }
-
-    next();
-  } catch (e) {
-    console.log(e);
-    res
-      .status(422)
-      .json({ message: "validation failed", error: (e as Error).message });
-  }
 };
