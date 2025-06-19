43aef2cbf78aeb5c45a56ed6393856c2697fe45a
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-19 12:58:36 +0100

    refactored my databaseconfig

--- src/shared/config/database_config.ts
+++ src/shared/config/database_config.ts
@@ -2,14 +2,13 @@ import { Sequelize } from "sequelize";
 import config from "./index";
 
 const database = new Sequelize(
-  config.database.name!,
-  config.database.username!,
-  config.database.password!,
+  config.database.name,
+  config.database.username,
+  config.database.password,
   {
-    host: config.database.host,
     dialect: "postgres",
-    port: config.database.port || 5432,
-    logging: console.log,
+    port: config.database.port,
+    logging: false,
     pool: {
       max: 5,
       min: 0,
