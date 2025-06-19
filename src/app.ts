b9116dbf9fe00359fc1026ba01be197be554392b
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-19 09:14:50 +0100

    configure app.ts to only accept route to localhost:5173

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import express from "express";
 import appConfig from "./shared/config";
 import database from "./shared/config/database_config";
 import cors from "cors";
+import dotenv from "dotenv";
 import rootRoutes from "./rootRoutes";
 import * as path from "node:path";
+
+dotenv.config();
 
 const app = express();
 
@@ -11,15 +14,15 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/public", express.static(path.join(__dirname,  "public",)));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"], // Allow all origins (for development only)
+    origin: process.env.ORIGIN_URL, // Allow all origins (for development only)
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
-  }),
+  })
 );
 
 // API ROUTES
@@ -27,8 +30,8 @@ app.use("/api", rootRoutes);
 
 app.listen(appConfig.app.port, appConfig.app.host, async () => {
   // await database.sync({force: true})
-  await database.sync({ alter:true});
+  await database.sync({ alter: true });
   console.log(
-    `app listening on ${appConfig.app.port} on ${appConfig.app.host}`,
+    `app listening on ${appConfig.app.port} on ${appConfig.app.host}`
   );
 });
