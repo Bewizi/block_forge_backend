b7728bbc4d35e82ec7fe65096780d380650eb64a
Bewizi <toluwalase.bewaji@yahoo.com>
2025-06-19 11:33:43 +0100

    undo codes

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,26 +1,26 @@
-import {ServerConfig} from '../types/server_types'
-import dotenv from 'dotenv'
+import { ServerConfig } from "../types/server_types";
+import dotenv from "dotenv";
 import * as process from "node:process";
 
-dotenv.config()
+dotenv.config();
 
 const getEnv = (name: string) => {
-    return process.env[name] ?? ""
-}
+  return process.env[name] ?? "";
+};
 
 const config: ServerConfig = {
-    app: {
-        name: getEnv("APP_NAME"),
-        host: getEnv("APP_HOST"),
-        port: Number(getEnv("APP_PORT"))
-    },
+  app: {
+    name: getEnv("APP_NAME"),
+    host: getEnv("APP_HOST"),
+    port: Number(getEnv("APP_PORT")),
+  },
 
-    database: {
-        name: getEnv("DATABASE_NAME"),
-        port: Number(getEnv("DATABASE_PORT")),
-        username: getEnv("DATABASE_USERNAME"),
-        password: getEnv("DATABASE_PASSWORD")
-    }
-}
+  database: {
+    name: getEnv("DATABASE_NAME"),
+    port: Number(getEnv("DATABASE_PORT")),
+    username: getEnv("DATABASE_USERNAME"),
+    password: getEnv("DATABASE_PASSWORD"),
+  },
+};
 
-export default config
+export default config;
